e842452000f089db5e43e02f652243135c63e23a
mamunabdullah232 <mamunabdullah262@gmail.com>
2026-05-23 20:27:14 +0530

    Add files via upload

---
+++ xohopathi_home_and_class_test/plantation/plants-data.js
@@ -0,0 +1,15 @@
+window.XOHOPATHI_PLANTS = [
+  {
+    id: "XPH-PLANT-001",
+    plantName: "Plant Name To Be Updated",
+    scientificName: "Scientific name to be updated",
+    donorName: "To be updated",
+    maintenancePerson: "To be updated",
+    plantationDate: "To be updated",
+    location: "JHSS Campus",
+    status: "Registration pending",
+    lastUpdated: "To be updated",
+    photo: "",
+    note: "This record is reserved for the first QR-labelled plant. Provide the actual plant and caretaker details to publish the completed record."
+  }
+];
