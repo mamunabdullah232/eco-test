223ab2c68792a4aa178128afbf71d2597a1d4528
mamunabdullah232 <mamunabdullah262@gmail.com>
2026-05-23 22:50:39 +0530

    Add files via upload

---
+++ plantation/plants-data.js
@@ -0,0 +1,15 @@
+window.XOHOPATHI_PLANTS = Array.from({ length: 25 }, function(_, index){
+  return {
+    id: "ZG" + String(index + 1).padStart(2, "0"),
+    plantName: "Plant species to be updated",
+    scientificName: "Record after plantation",
+    donorName: "To be updated",
+    maintenancePerson: "To be updated",
+    plantationDate: "28 May 2026",
+    location: "Niz Baruajhar",
+    status: "Planned for plantation",
+    lastUpdated: "23 May 2026",
+    photo: "",
+    note: "QR label prepared before the plantation drive. Plant species, caretaker and photograph will be recorded after planting."
+  };
+});
